12a609a883501029b2dc11231ff87a3c7fd487d4
Thauron94 <35653075+Thauron94@users.noreply.github.com>
2018-03-07 09:58:56 +0200

    Update and rename backup_ver1.py to backup_ver3.py

--- backup/backup_ver3.py
+++ backup/backup_ver3.py
@@ -4,31 +4,45 @@ import time
 # 1. The files and directories to be backed up are
 # specified in a list.
 # Example on Windows:
-source = ['"C:\\My Documents"']
-# or source = r'C:\My Documents'
+source = r'C:\My Documents'
 # Example on Mac OS X and Linux:
 # source = ['/Users/swa/notes']
 # Notice we have to use double quotes inside a string
-# for names with spaces in it. We could have also used 
-# a raw string by writing [r'C:\My Documents'].
+# for names with spaces in it.
 
 # 2. The backup must be stored in a 
 # main backup directory
 # Example on Windows:
-target_dir = 'E:\\Backup'
-# or target_dir = r'E:\Backup'
+target_dir = r'E:\Backup'
 # Example on Mac OS X and Linux:
 # target_dir = '/Users/swa/backup'
 # Remember to change this to which folder you will be using
 
-# 3. The files are backed up into a zip file.
-# 4. The name of the zip archive is the current date and time
-target = taget_dir + os.sep + \
-         time.strftime('%Y%m%d%H%M%S') + '.zip'
-    
 # Create target directory if it is not present
 if not os.path.exist(target_dir):
     os.mkdir(target_dir)  # make directory
+    
+# 3. The files are backed up into a zip file.
+# 4. The current day is the name of the subdirectory
+# in the main directory.
+today = target_dir + os.sep + time.strftime('%Y%m%d')
+# The current time is the name of the zip archive.
+now = time.strftime('%H%M%S')
+
+# Take a comment from the user to 
+# create the name of the zip file
+comment = input('Enter a comment --> ')
+# Check if a comment was entered 
+if len(comment) == 0:
+    target = today + os.sep + now + '.zip'
+else:
+    target = today + os.sep + now + '_' +
+        comment.replace(' ', '_') + '.zip'
+        
+# Create the subdirectory if it is isn't already there
+if not os.path.exists(today):
+    os.mkdir(today)
+    print('Successfully created directory', today)
     
 # 5. We use the zip command to put the files in a zip archive
 zip_command = 'zip -r {0} {1}'.format(target,
